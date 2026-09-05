0cde505236a9d7a8796b64394f0593c368cf37ee
JustinGojar <gojarjustinofficial@gmail.com>
2026-09-05 21:40:24 +0800

    feat: update email header to display brand name instead of logo for improved clarity

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,8 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ rtrim(config('app.frontend_url'), '/') }}/logo.png" class="logo" alt="Bulan SeniorCare Logo">
+<a href="{{ $url }}" style="display: inline-block; color: #2f3a4d; font-size: 22px; font-weight: 700; text-decoration: none;">
+Bulan Senior Care
 </a>
 </td>
 </tr>
